3e1a75d2be2cb5d2a642ee8c94ed58f50eb5f7c1
alexy piche <alexyautumn@gmail.com>
2024-05-09 12:29:14 -0400

    implement simple project page + some responsiveness

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,11 +2,29 @@ function toggleDarkMode() {
     const checkbox = document.getElementById('darkModeCheckbox');
     const body = document.body;
 
+    // Function to set theme
+    function setTheme(theme) {
+        body.setAttribute('data-bs-theme', theme);
+        // Store the theme preference in local storage
+        localStorage.setItem('theme', theme);
+    }
+
+    // Check if theme preference exists in local storage
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+        // Apply the stored theme
+        setTheme(storedTheme);
+        // Update checkbox state accordingly
+        checkbox.checked = storedTheme === 'light';
+    }
+
+    // Event listener for checkbox change
     checkbox.addEventListener('change', function() {
         var theme = checkbox.checked ? 'light' : 'dark';
-        body.setAttribute('data-bs-theme', theme);
+        setTheme(theme);
     });
 }
+
 
 toggleDarkMode();
 
@@ -22,13 +40,28 @@ $(window).scroll(function() {
 const options_f = { 
     layout: 'sameSize',
     gutterPixels: 20,
+    selector: '.grid-cus',
     gridItemsSelector: '.filtr-item',
+    spinner: {
+        // This is the only mandatory setting if you simply wish
+        // to enable the built-in spinner
+        enabled: true,
+        // Further (optional) customization options.
+        fillColor: '#ffffff',
+        styles: {
+          height: '75px',
+          margin: '0 auto',
+          width: '75px',
+          'z-index': 2,
+        },
+      }
 };
+document.addEventListener('DOMContentLoaded', function() {
 
 // Adjust the CSS selector to match the container where
 // you set up your image gallery
 const filterizr = new Filterizr('.grid-cus', options_f);
-
+});
 
   
 Fancybox.bind("[data-fancybox]", {
